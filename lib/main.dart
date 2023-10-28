62b89c0abc76b2d07aa5c5f148f019ab093b9490
Snippy <acc.snippy@tutanota.com>
2023-10-28 18:37:40 +0200

    Added NavBar and corresponding Pages.

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,104 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  var index = 0;
+  final pages = [
+    const TasksPage(),
+    const NewTasksPage(),
+    const SettingsPage(),
+  ];
+
+  
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+            // App Bar
+            appBar: AppBar(
+                    title: const Text('ToDo'),
+                    backgroundColor: Color.fromARGB(255, 255, 34, 0) ,
+            ),
+            
+            // Body
+            body: pages[index],
+
+            // Buttom Navigatin Bar
+            bottomNavigationBar: NavigationBar(                                                                     
+                                              selectedIndex: index,
+                                              onDestinationSelected: (i) => 
+                                                                    setState(() => index = i),
+                                              destinations: const [ NavigationDestination(icon: Icon(Icons.calendar_month_outlined), label: 'Tasks'),
+                                                                     NavigationDestination(icon: Icon(Icons.add_circle_outline), label: 'New'),
+                                                                     NavigationDestination(icon: Icon(Icons.settings_outlined), label: 'Settings'),
+                                                                  ],
+                                              labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+                                              height: 50,   
+                                              indicatorColor: Color.fromARGB(255, 255, 34, 0)         
+                                               ),
+            
       ),
     );
+  }
+}
+
+
+
+
+
+// Home, Tasks Page
+class TasksPage extends StatelessWidget {
+  const TasksPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return (const Scaffold(
+      body: Text('Tasks'),
+    ));
+  }
+}
+
+
+
+
+// Add new task page
+class NewTasksPage extends StatelessWidget {
+  const NewTasksPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return(const Scaffold(
+      body: Text('New'),
+    ));
+  }
+}
+
+
+
+
+// Settings
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return (const Scaffold(
+      body: Text('Settings'),
+    ));
   }
 }
