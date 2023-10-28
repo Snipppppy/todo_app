0e693295d1b85f60a2ca7c55a688d07ac6081a1c
Snippy <acc.snippy@tutanota.com>
2023-10-28 19:18:19 +0200

    Pages are now each lokatet in seperate files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/newtask_page.dart';
+import 'package:todo_app/settings_page.dart';
+import 'package:todo_app/tasks_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -18,7 +21,6 @@ class _MainAppState extends State<MainApp> {
     const NewTasksPage(),
     const SettingsPage(),
   ];
-
   
   @override
   Widget build(BuildContext context) {
@@ -29,7 +31,7 @@ class _MainAppState extends State<MainApp> {
             // App Bar
             appBar: AppBar(
                     title: const Text('ToDo'),
-                    backgroundColor: Color.fromARGB(255, 255, 34, 0) ,
+                    backgroundColor: const Color.fromARGB(255, 255, 34, 0) ,
             ),
             
             // Body
@@ -46,62 +48,9 @@ class _MainAppState extends State<MainApp> {
                                                                   ],
                                               labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
                                               height: 50,   
-                                              indicatorColor: Color.fromARGB(255, 255, 34, 0)         
-                                               ),
-            
+                                              indicatorColor: const Color.fromARGB(255, 255, 34, 0)         
+                                               ),            
       ),
     );
-  }
-}
-
-
-
-
-
-// Home, Tasks Page
-class TasksPage extends StatelessWidget {
-  const TasksPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return (const Scaffold(
-      body: Text('Tasks'),
-    ));
-  }
-}
-
-
-
-
-// Add new task page
-class NewTasksPage extends StatelessWidget {
-  const NewTasksPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return(const Scaffold(
-      body: Text('New'),
-    ));
-  }
-}
-
-
-
-
-// Settings
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return (const Scaffold(
-      body: Text('Settings'),
-    ));
   }
 }
---
+++ lib/newTask_page.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class NewTasksPage extends StatelessWidget {
+  const NewTasksPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return(const Scaffold(
+      body: Text('NewTask from a Seperate page file.'),
+    ));
+  }
+}
